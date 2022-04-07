810e797a2037b3731094ab7c22bf19b1204f30a3
Pony Yan <yankfy@qq.com>
2022-04-07 17:51:24 +0800

    chore: 修改prettier配置

--- .eslintrc.js
+++ .eslintrc.js
@@ -7,6 +7,16 @@ module.exports = {
     rules: {
         'vue/no-unused-vars': 'error',
         'vue/no-multiple-template-root': 'off',
-        "no-unused-expressions": "off"
+        "no-unused-expressions": "off",
+        "vue/require-default-prop": "off",
     },
+    // 'parser': '@typescript-eslint/parser',
+    // 'parserOptions': {
+    //     'parser': 'babel-eslint',
+    // },
+    "parser": "vue-eslint-parser",
+    "parserOptions": {
+        "parser": "@typescript-eslint/parser"
+    }
+
 }
