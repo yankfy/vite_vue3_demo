0361d28bf9df0d5b13e53a96ee43dbcadace1df8
yankfy <yankfy@qq.com>
2022-06-07 14:44:03 +0800

    chore: ts 校验

--- src/runtime.d.ts
+++ src/runtime.d.ts
@@ -1,9 +1,9 @@
 // 挂载到vue实例上
 import { ElMessage, ElMessageBox, ElNotification } from "element-plus";
-import { Size } from './plugins/element'
+import { Size } from "./plugins/element";
 
 // vue实例上挂载属性类型声明
-declare module '@vue/runtime-core' {
+declare module "@vue/runtime-core" {
     interface ComponentCustomProperties {
         $message: typeof ElMessage;
         $notification: typeof ElNotification;
@@ -12,7 +12,7 @@ declare module '@vue/runtime-core' {
         $prompt: typeof ElMessageBox.prompt;
         $ELEMENT: {
             size: Size;
-        }
+        };
     }
 }
 
